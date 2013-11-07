54b3b749d31b617907806896aee684f5cbc3bceb
unknown <jose.melo@ccc.ufcg.edu.br>
2013-11-07 15:22:28 -0200

    1.0.0.1

---
+++ lab1/NumberText/src/NumberText.java
@@ -0,0 +1,13 @@
+
+public class NumberText {
+	
+	public static String getNumberText(String number) {
+		
+		if (number.equals("0")) {
+			return "zero";
+		}
+		
+		return number.equals("1")? "um":"dois";
+	}
+
+}
