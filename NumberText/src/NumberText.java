b90073f4ada0adc90c93476f846e7135744ad7e6
unknown <jose.melo@ccc.ufcg.edu.br>
2013-11-07 15:43:12 -0200

    1.0.0.2

--- NumberText/src/NumberText.java
+++ NumberText/src/NumberText.java
@@ -2,8 +2,12 @@
 public class NumberText {
 	
 	public static String getNumberText(String number) {
-		return "zero";
 		
+		if (number.equals("0")) {
+			return "zero";
+		}
+		
+		return number.equals("1")? "um":"dois";
 	}
 
 }
--- NumberText/src/TextNumberText.java
+++ NumberText/src/TextNumberText.java
@@ -6,8 +6,14 @@ import org.junit.Test;
 public class TextNumberText {
 	
 	@Test
-	public void testa0(){
+	public void testa0_9(){
 		
-		assertEquals("zero", NumberText.getNumberText("0"));
-	}
+		String[] unidades = {"zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", 
+				             "nove"};
+	
+		for(int i = 0; i > unidades.length; i++) {
+			assertEquals(unidades[i], NumberText.getNumberText(i+""));
+		}
+	}	
+
 }
